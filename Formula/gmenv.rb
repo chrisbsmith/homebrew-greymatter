f264a3012339ef75ed03e3da6fd9fdc51897631b
Chris Smith <chris.b.smith@gmail.com>
2020-05-21 22:52:53 -0400

    wip

--- Formula/gmenv.rb
+++ Formula/gmenv.rb
@@ -16,9 +16,9 @@ class Gmenv < Formula
 
   def post_install
     versions_path = HOMEBREW_PREFIX/"etc/#{name}/versions"
-    versions_local = buildpath/"versions"
+    #versions_local = buildpath/"versions"
 
-    mkdir_p versions_path unless versions_local.directory?
+    mkdir_p versions_path unless versions_path.directory?
 
     #mkdir_p versions_local
     #ln_s versions_path, versions_local
